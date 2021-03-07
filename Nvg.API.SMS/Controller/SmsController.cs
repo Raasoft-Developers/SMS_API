fbbe4a2cc76fc2412ba91db0de05d0b896a52183
Abhishek Puranik <Abhishek.Puranik@novigosolutions.com>
2021-03-07 15:19:22 +0530

    Added logging in SMS controller and SMS interactor and changed log filename

--- Nvg.API.SMS/Controller/SmsController.cs
+++ Nvg.API.SMS/Controller/SmsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nvg.SMSService.Data;
 using Nvg.SMSService.Data.Entities;
 using Nvg.SMSService.DTOS;
@@ -17,90 +18,212 @@ namespace Nvg.API.SMS.Controller
     public class SmsController : ControllerBase
     {
         private readonly ISMSInteractor _smsInteractor;
+        private readonly ILogger<SmsController> _logger;
 
-        public SmsController(ISMSInteractor smsInteractor)
+        public SmsController(ISMSInteractor smsInteractor, ILogger<SmsController> logger)
         {
             _smsInteractor = smsInteractor;
+            _logger = logger;
         }
 
         [HttpPost]
         public ActionResult AddSMSPool(SMSPoolDto poolInput)
         {
-            var poolResponse = _smsInteractor.AddSMSPool(poolInput);
-            if (poolResponse.Status)
-                return Ok(poolResponse);
-            else
-                return StatusCode(412, poolResponse);
+            _logger.LogInformation("In SmsController: AddSMSPool action method hit.");
+            try
+            {
+                var poolResponse = _smsInteractor.AddSMSPool(poolInput);
+                if (poolResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + poolResponse.Message);
+                    return Ok(poolResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + poolResponse.Message);
+                    return StatusCode(412, poolResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while adding SMS pool: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPost]
         public ActionResult AddSMSProvider(SMSProviderSettingsDto providerInput)
         {
-            var providerResponse = _smsInteractor.AddSMSProvider(providerInput);
-            if (providerResponse.Status)
-                return Ok(providerResponse);
-            else
-                return StatusCode(412, providerResponse);
+            _logger.LogInformation("In SmsController: AddSMSProvider action method hit.");
+            try
+            {
+                var providerResponse = _smsInteractor.AddSMSProvider(providerInput);
+                if (providerResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + providerResponse.Message);
+                    return Ok(providerResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + providerResponse.Message);
+                    return StatusCode(412, providerResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while adding SMS provider: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPost]
         public ActionResult AddSMSChannel(SMSChannelDto channelInput)
         {
-            var channelResponse = _smsInteractor.AddSMSChannel(channelInput);
-            if (channelResponse.Status)
-                return Ok(channelResponse);
-            else
-                return StatusCode(412, channelResponse);
+            _logger.LogInformation("In SmsController: AddSMSChannel action method hit.");
+            try
+            {
+                var channelResponse = _smsInteractor.AddSMSChannel(channelInput);
+                if (channelResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + channelResponse.Message);
+                    return Ok(channelResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + channelResponse.Message);
+                    return StatusCode(412, channelResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while adding SMS channel: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPost]
         public ActionResult AddSMSTemplate(SMSTemplateDto templateInput)
         {
-            var templateResponse = _smsInteractor.AddSMSTemplate(templateInput);
-            if (templateResponse.Status)
-                return Ok(templateResponse);
-            else
-                return StatusCode(412, templateResponse);
+            _logger.LogInformation("In SmsController: AddSMSTemplate action method hit.");
+            try
+            {
+                var templateResponse = _smsInteractor.AddSMSTemplate(templateInput);
+                if (templateResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + templateResponse.Message);
+                    return Ok(templateResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + templateResponse.Message);
+                    return StatusCode(412, templateResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while adding SMS template: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpGet("{channelKey}")]
         public ActionResult GetSMSChannelByKey(string channelKey)
         {
-            var channelResponse = _smsInteractor.GetSMSChannelByKey(channelKey);
-            if (channelResponse.Status)
-                return Ok(channelResponse);
-            else
-                return StatusCode(412, channelResponse);
+            _logger.LogInformation("In SmsController: GetSMSChannelByKey action method hit.");
+            try
+            {
+                var channelResponse = _smsInteractor.GetSMSChannelByKey(channelKey);
+                if (channelResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + channelResponse.Message);
+                    return Ok(channelResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + channelResponse.Message);
+                    return StatusCode(412, channelResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while getting SMS channel by key: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpGet("{poolName}/{providerName}")]
         public ActionResult GetSMSProvidersByPool(string poolName, string providerName)
         {
-            var poolResponse = _smsInteractor.GetSMSProvidersByPool(poolName, providerName);
-            if (poolResponse.Status)
-                return Ok(poolResponse);
-            else
-                return StatusCode(412, poolResponse);
+            _logger.LogInformation("In SmsController: GetSMSProvidersByPool action method hit.");
+            try
+            {
+                var poolResponse = _smsInteractor.GetSMSProvidersByPool(poolName, providerName);
+                if (poolResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + poolResponse.Message);
+                    return Ok(poolResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + poolResponse.Message);
+                    return StatusCode(412, poolResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while getting SMS providers by pool: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpGet("{channelKey}/{tag?}")]
         public ActionResult GetSMSHistories(string channelKey, string tag = null)
         {
-            var historiesResponse = _smsInteractor.GetSMSHistoriesByTag(channelKey, tag);
-            if (historiesResponse.Status)
-                return Ok(historiesResponse);
-            else
-                return StatusCode(412, historiesResponse);
+            _logger.LogInformation("In SmsController: GetSMSHistories action method hit.");
+            try
+            {
+                var historiesResponse = _smsInteractor.GetSMSHistoriesByTag(channelKey, tag);
+                if (historiesResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + historiesResponse.Message);
+                    return Ok(historiesResponse);
+                }
+                else
+                {
+                    _logger.LogError("In SmsController: " + historiesResponse.Message);
+                    return StatusCode(412, historiesResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while getting SMS histories: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
 
         [HttpPost]
         public ActionResult SendSMS(SMSDto smsInputs)
         {
-            var smsResponse = _smsInteractor.SendSMS(smsInputs);
-            if(smsResponse.Status)
-                return Ok(smsResponse);
+            _logger.LogInformation("In SmsController: SendSMS action method hit.");
+            try
+            {
+                var smsResponse = _smsInteractor.SendSMS(smsInputs);
+            if (smsResponse.Status)
+                {
+                    _logger.LogDebug("In SmsController: " + smsResponse.Message);
+                    return Ok(smsResponse);
+                }
             else
-                return StatusCode(412, smsResponse);
+                {
+                    _logger.LogError("In SmsController: " + smsResponse.Message);
+                    return StatusCode(412, smsResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SmsController: Internal server error: Error occurred while sending SMS: " + ex.Message);
+                return StatusCode(500, ex);
+            }
         }
     }
 }
--- Nvg.SMSService/SMS/SMSInteractor.cs
+++ Nvg.SMSService/SMS/SMSInteractor.cs
@@ -1,4 +1,5 @@
 ﻿using EventBus.Abstractions;
+using Microsoft.Extensions.Logging;
 using Nvg.SMSService.DTOS;
 using Nvg.SMSService.Events;
 using Nvg.SMSService.SMSChannel;
@@ -19,11 +20,12 @@ namespace Nvg.SMSService.SMS
         private readonly ISMSChannelInteractor _smsChannelInteractor;
         private readonly ISMSTemplateInteractor _smsTemplateInteractor;
         private readonly ISMSHistoryInteractor _smsHistoryInteractor;
+        private readonly ILogger<SMSInteractor> _logger;
 
-        public SMSInteractor(ISMSEventInteractor smsEventInteractor, 
+        public SMSInteractor(ISMSEventInteractor smsEventInteractor,
             ISMSPoolInteractor smsPoolInteractor, ISMSProviderInteractor smsProviderInteractor,
             ISMSChannelInteractor smsChannelInteractor, ISMSTemplateInteractor smsTemplateInteractor,
-            ISMSHistoryInteractor smsHistoryInteractor)
+            ISMSHistoryInteractor smsHistoryInteractor, ILogger<SMSInteractor> logger)
         {
             _smsEventInteractor = smsEventInteractor;
             _smsPoolInteractor = smsPoolInteractor;
@@ -31,57 +33,152 @@ namespace Nvg.SMSService.SMS
             _smsChannelInteractor = smsChannelInteractor;
             _smsTemplateInteractor = smsTemplateInteractor;
             _smsHistoryInteractor = smsHistoryInteractor;
+            _logger = logger;
         }
 
         public SMSResponseDto<SMSPoolDto> AddSMSPool(SMSPoolDto poolInput)
         {
-            var poolResponse = _smsPoolInteractor.AddSMSPool(poolInput);
-            return poolResponse;
+            _logger.LogInformation("In SMSInteractor: AddSMSPool interactor method hit.");
+            SMSResponseDto<SMSPoolDto> poolResponse = new SMSResponseDto<SMSPoolDto>();
+            try
+            {
+                poolResponse = _smsPoolInteractor.AddSMSPool(poolInput);
+                _logger.LogDebug("In SMSInteractor: " + poolResponse.Message);
+                return poolResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while adding SMS pool: ", ex.Message);
+                poolResponse.Message = "Error occurred while adding SMS pool: " + ex.Message;
+                poolResponse.Status = false;
+                return poolResponse;
+            }
         }
 
         public SMSResponseDto<SMSProviderSettingsDto> AddSMSProvider(SMSProviderSettingsDto providerInput)
         {
-            var providerResponse = _smsProviderInteractor.AddSMSProvider(providerInput);
-            return providerResponse;
+            _logger.LogInformation("In SMSInteractor: AddSMSProvider interactor method hit.");
+            SMSResponseDto<SMSProviderSettingsDto> providerResponse = new SMSResponseDto<SMSProviderSettingsDto>();
+            try
+            {
+                providerResponse = _smsProviderInteractor.AddSMSProvider(providerInput);
+                _logger.LogDebug("In SMSInteractor: " + providerResponse.Message);
+                return providerResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while adding SMS provider: ", ex.Message);
+                providerResponse.Message = "Error occurred while adding SMS provider: " + ex.Message;
+                providerResponse.Status = false;
+                return providerResponse;
+            }
         }
 
         public SMSResponseDto<SMSChannelDto> AddSMSChannel(SMSChannelDto channelInput)
         {
-            var channelResponse = _smsChannelInteractor.AddSMSChannel(channelInput);
-            return channelResponse;
+            _logger.LogInformation("In SMSInteractor: AddSMSChannel interactor method hit.");
+            SMSResponseDto<SMSChannelDto> channelResponse = new SMSResponseDto<SMSChannelDto>();
+            try
+            {
+                channelResponse = _smsChannelInteractor.AddSMSChannel(channelInput);
+                _logger.LogDebug("In SMSInteractor: " + channelResponse.Message);
+                return channelResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while adding SMS channel: ", ex.Message);
+                channelResponse.Message = "Error occurred while adding SMS channel: " + ex.Message;
+                channelResponse.Status = false;
+                return channelResponse;
+            }
         }
 
         public SMSResponseDto<SMSTemplateDto> AddSMSTemplate(SMSTemplateDto templateInput)
         {
-            var templateResponse = _smsTemplateInteractor.AddSMSTemplate(templateInput);
-            return templateResponse;
+            _logger.LogInformation("In SMSInteractor: AddSMSTemplate interactor method hit.");
+            SMSResponseDto<SMSTemplateDto> templateResponse = new SMSResponseDto<SMSTemplateDto>();
+            try
+            {
+                templateResponse = _smsTemplateInteractor.AddSMSTemplate(templateInput);
+                _logger.LogDebug("In SMSInteractor: " + templateResponse.Message);
+                return templateResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while adding SMS template: ", ex.Message);
+                templateResponse.Message = "Error occurred while adding SMS template: " + ex.Message;
+                templateResponse.Status = false;
+                return templateResponse;
+            }
         }
 
         public SMSResponseDto<SMSChannelDto> GetSMSChannelByKey(string channelKey)
         {
-            var channelResponse = _smsChannelInteractor.GetSMSChannelByKey(channelKey);
-            return channelResponse;
+            _logger.LogInformation("In SMSInteractor: GetSMSChannelByKey interactor method hit.");
+            SMSResponseDto<SMSChannelDto> channelResponse = new SMSResponseDto<SMSChannelDto>();
+            try
+            {
+                channelResponse = _smsChannelInteractor.GetSMSChannelByKey(channelKey);
+                _logger.LogDebug("In SMSInteractor: " + channelResponse.Message);
+                return channelResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while getting SMS channel by key: ", ex.Message);
+                channelResponse.Message = "Error occurred while getting SMS channel by key: " + ex.Message;
+                channelResponse.Status = false;
+                return channelResponse;
+            }
         }
 
         public SMSResponseDto<List<SMSProviderSettingsDto>> GetSMSProvidersByPool(string poolName, string providerName)
         {
-            var poolResponse = _smsProviderInteractor.GetSMSProvidersByPool(poolName, providerName);
-            return poolResponse;
+            _logger.LogInformation("In SMSInteractor: GetSMSProvidersByPool interactor method hit.");
+            SMSResponseDto<List<SMSProviderSettingsDto>> poolResponse = new SMSResponseDto<List<SMSProviderSettingsDto>>();
+            try
+            {
+                poolResponse = _smsProviderInteractor.GetSMSProvidersByPool(poolName, providerName);
+                _logger.LogDebug("In SMSInteractor: " + poolResponse.Message);
+                return poolResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while getting SMS provider by pool: ", ex.Message);
+                poolResponse.Message = "Error occurred while getting SMS provider by pool: " + ex.Message;
+                poolResponse.Status = false;
+                return poolResponse;
+            }
         }
 
         public SMSResponseDto<List<SMSHistoryDto>> GetSMSHistoriesByTag(string channelKey, string tag)
         {
-            var poolResponse = _smsHistoryInteractor.GetSMSHistoriesByTag(channelKey, tag);
-            return poolResponse;
+            _logger.LogInformation("In SMSInteractor: GetSMSHistoriesByTag interactor method hit.");
+            SMSResponseDto<List<SMSHistoryDto>> poolResponse = new SMSResponseDto<List<SMSHistoryDto>>();
+            try
+            {
+                poolResponse = _smsHistoryInteractor.GetSMSHistoriesByTag(channelKey, tag);
+                _logger.LogDebug("In SMSInteractor: " + poolResponse.Message);
+                return poolResponse;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("In SMSInteractor: Error occurred in SMS Interactor while getting SMS histories: ", ex.Message);
+                poolResponse.Message = "Error occurred while getting SMS histories: " + ex.Message;
+                poolResponse.Status = false;
+                return poolResponse;
+            }
         }
 
         public SMSResponseDto<string> SendSMS(SMSDto smsInputs)
         {
+
+            _logger.LogInformation("In SMSInteractor: SendSMS interactor method hit.");
             var response = new SMSResponseDto<string>();
             try
             {
                 if (string.IsNullOrEmpty(smsInputs.ChannelKey))
                 {
+                    _logger.LogError("In SMSInteractor: Channel key cannot be blank.");
                     response.Status = false;
                     response.Message = "Channel key cannot be blank.";
                     return response;
@@ -89,8 +186,9 @@ namespace Nvg.SMSService.SMS
                 else
                 {
                     var channelExist = _smsChannelInteractor.CheckIfChannelExist(smsInputs.ChannelKey).Result;
-                    if(!channelExist)
+                    if (!channelExist)
                     {
+                        _logger.LogError($"In SMSInteractor: Invalid Channel key {smsInputs.ChannelKey}.");
                         response.Status = channelExist;
                         response.Message = $"Invalid Channel key {smsInputs.ChannelKey}.";
                         return response;
@@ -98,6 +196,7 @@ namespace Nvg.SMSService.SMS
                 }
                 if (string.IsNullOrEmpty(smsInputs.TemplateName))
                 {
+                    _logger.LogError($"In SMSInteractor: Template name cannot be blank.");
                     response.Status = false;
                     response.Message = "Template name cannot be blank.";
                     return response;
@@ -107,6 +206,7 @@ namespace Nvg.SMSService.SMS
                     var templateExist = _smsTemplateInteractor.CheckIfTemplateExist(smsInputs.ChannelKey, smsInputs.TemplateName).Result;
                     if (!templateExist)
                     {
+                        _logger.LogError($"In SMSInteractor: No template found for template name {smsInputs.TemplateName} and channel key {smsInputs.ChannelKey}.");
                         response.Status = templateExist;
                         response.Message = $"No template found for template name {smsInputs.TemplateName} and channel key {smsInputs.ChannelKey}.";
                         return response;
@@ -115,10 +215,12 @@ namespace Nvg.SMSService.SMS
                 _smsEventInteractor.SendSMS(smsInputs);
                 response.Status = true;
                 response.Message = $"SMS is sent successfully to {smsInputs.Recipients}.";
+                _logger.LogDebug("In SMSInteractor: " + response.Message);
                 return response;
             }
             catch (Exception ex)
             {
+                _logger.LogError("In SMSInteractor: Error occurred in SMSInteractor while sending SMS: ", ex.Message);
                 response.Status = false;
                 response.Message = ex.Message;
                 return response;
