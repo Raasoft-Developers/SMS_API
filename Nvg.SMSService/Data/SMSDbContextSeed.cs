177e325db5c088b0d57d965926e9a8d45ffa9d0c
Abhishek Puranik <Abhishek.Puranik@novigosolutions.com>
2021-06-04 11:27:12 +0530

    Updated seeding data, logic and rabbitmq event bus url ***NO_CI***

--- Nvg.SMSService/Data/SMSDbContextSeed.cs
+++ Nvg.SMSService/Data/SMSDbContextSeed.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Nvg.SMSService.Data.Entities;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -46,17 +47,18 @@ namespace Nvg.SMSService.Data
                 {
                     foreach (var channel in smsChannelData)
                     {
-                        var name = (string)channel.name;
-                        var smsPoolID = (string)channel.smsPoolID;
-                        var smsChannelFromTblHasValue = context.SMSChannels.Any(s => s.Key == name && s.SMSPoolID == smsPoolID);
+                        var key = (string)channel.key;
+                        var smsPoolName = (string)channel.smsPoolName;
+                        var smsPoolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
+                        var smsChannelFromTblHasValue = context.SMSChannels.Any(s => s.Key == key && s.SMSPoolID == smsPoolID);
                         if (!smsChannelFromTblHasValue)
-                            SeedSMSChannels(context, channel);
+                            SeedSMSChannels(context, channel, smsPoolID);
                     }
                 }
                 else
                 {
                     foreach (var channel in smsChannelData)
-                        SeedSMSChannels(context, channel);
+                        SeedSMSChannels(context, channel, null);
                 }
             }
         }
@@ -71,16 +73,17 @@ namespace Nvg.SMSService.Data
                     {
                         var name = (string)template.name;
                         var messageTemplate = (string)template.messageTemplate;
-                        var smsPoolID = (string)template.smsPoolID;
+                        var smsPoolName = (string)template.smsPoolName;
+                        var smsPoolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
                         var smsTemplateFromTblHasValue = context.SMSTemplates.Any(s => s.Name == name && s.SMSPoolID == smsPoolID && s.MessageTemplate == messageTemplate);
                         if (!smsTemplateFromTblHasValue)
-                            SeedSMSTemplates(context, template);
+                            SeedSMSTemplates(context, template, smsPoolID);
                     }
                 }
                 else
                 {
                     foreach (var template in smsTemplatesData)
-                        SeedSMSTemplates(context, template);
+                        SeedSMSTemplates(context, template, null);
                 }
             }
         }
@@ -95,17 +98,17 @@ namespace Nvg.SMSService.Data
                     {
                         var name = (string)provider.name;
                         var configuration = (string)provider.configuration;
-                        var smsPoolID = (string)provider.smsPoolID;
-                        var id = (string)provider.id;
-                        var smsProviderFromTblHasValue = context.SMSProviderSettings.Any(s => s.ID == id && s.SMSPoolID == smsPoolID);
+                        var smsPoolName = (string)provider.smsPoolName;
+                        var smsPoolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
+                        var smsProviderFromTblHasValue = context.SMSProviderSettings.Any(s => s.Name == name && s.SMSPoolID == smsPoolID);
                         if (!smsProviderFromTblHasValue)
-                            SeedSMSProviders(context, provider);
+                            SeedSMSProviders(context, provider, smsPoolID);
                     }
                 }
                 else
                 {
                     foreach (var provider in smsProvidersData)
-                        SeedSMSProviders(context, provider);
+                        SeedSMSProviders(context, provider, null);
                 }
             }
         }
@@ -114,47 +117,63 @@ namespace Nvg.SMSService.Data
         {
             context.SMSPools.Add(new SMSPoolTable
             {
-                ID = smsPool.id,
+                ID = Guid.NewGuid().ToString(),
                 Name = smsPool.name,
             });
             context.SaveChanges();
         }
 
-        private static void SeedSMSChannels(SMSDbContext context, dynamic smsChannel)
+        private static void SeedSMSChannels(SMSDbContext context, dynamic smsChannel,string poolID)
         {
+            if (string.IsNullOrEmpty(poolID)) {
+                var smsPoolName = (string)smsChannel.smsPoolName;
+                poolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
+            }
+            var smsProviderName = (string)smsChannel.smsProviderName;
+            var providerID = context.SMSProviderSettings.Where(o => o.Name.ToLower().Equals(smsProviderName.ToLower())).Select(o => o.ID).FirstOrDefault();
             context.SMSChannels.Add(new SMSChannelTable
             {
-                ID = smsChannel.id,
-                Key = smsChannel.name,
-                SMSPoolID = smsChannel.smsPoolID,
-                SMSProviderID = smsChannel.smsProviderID
+                ID = Guid.NewGuid().ToString(),
+                Key = smsChannel.key,
+                SMSPoolID =poolID,
+                SMSProviderID = providerID
             });
             context.SaveChanges();
         }
 
-        private static void SeedSMSTemplates(SMSDbContext context, dynamic smsTemplate)
+        private static void SeedSMSTemplates(SMSDbContext context, dynamic smsTemplate, string poolID)
         {
+            if (string.IsNullOrEmpty(poolID))
+            {
+                var smsPoolName = (string)smsTemplate.smsPoolName;
+                poolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
+            }
             context.SMSTemplates.Add(new SMSTemplateTable
             {
-                ID = smsTemplate.id,
+                ID = Guid.NewGuid().ToString(),
                 Name = smsTemplate.name,
                 MessageTemplate = smsTemplate.messageTemplate,
                 Variant = smsTemplate.variant,
                 Sender = smsTemplate.sender,
-                SMSPoolID = smsTemplate.smsPoolID
+                SMSPoolID =poolID
             });
             context.SaveChanges();
         }
 
-        private static void SeedSMSProviders(SMSDbContext context, dynamic smsProvider)
+        private static void SeedSMSProviders(SMSDbContext context, dynamic smsProvider, string poolID)
         {
+            if (string.IsNullOrEmpty(poolID))
+            {
+                var smsPoolName = (string)smsProvider.smsPoolName;
+                poolID = context.SMSPools.Where(o => o.Name.ToLower().Equals(smsPoolName.ToLower())).Select(o => o.ID).FirstOrDefault();
+            }
             context.SMSProviderSettings.Add(new SMSProviderSettingsTable
             {
-                ID = smsProvider.id,
+                ID = Guid.NewGuid().ToString(),
                 Name = smsProvider.name,
                 Configuration = smsProvider.configuration,
                 Type = smsProvider.type,
-                SMSPoolID = smsProvider.smsPoolID,
+                SMSPoolID = poolID,
                 IsDefault = smsProvider.isDefault
             });
             context.SaveChanges();
