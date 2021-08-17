856d4a24abe8ab8c4c35bd79f75d001a1aafec2b
Abhishek Puranik <Abhishek.Puranik@novigosolutions.com>
2021-08-17 16:05:31 +0530

    Added sql server migrations ***NO_CI***

--- Nvg.SMSService/Data/Migrations/SqlServerMigrations/20210817103451_AddedTotalQuotaAndCurrentMonthInSMSQuota.Designer.cs
+++ Nvg.SMSService/Data/Migrations/SqlServerMigrations/20210817103451_AddedTotalQuotaAndCurrentMonthInSMSQuota.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nvg.SMSService.Data;
 
 namespace Nvg.SMSService.data.Migrations.SqlServerMigrations
 {
     [DbContext(typeof(SMSSqlServerDbContext))]
-    partial class SMSSqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210817103451_AddedTotalQuotaAndCurrentMonthInSMSQuota")]
+    partial class AddedTotalQuotaAndCurrentMonthInSMSQuota
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
